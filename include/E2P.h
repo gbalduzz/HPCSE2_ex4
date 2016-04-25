e131030adbe42caed722999ddccf0a97fad3c702
Giovanni <gbalduzz@itp.phys.ethz.ch>
2016-04-25 18:00:01 +0200

    naive implementation

--- include/E2P.h
+++ include/E2P.h
@@ -1,1 +1,10 @@
 #pragma once
+#include "particles.h"
+#include <cmath>
+
+double E2P(Complex z,const std::vector<Complex>& a){ //a[0]=Q
+
+    double result= (a[0]*std::log(z)).real();
+    for(int k=1;k<a.size();k++) result+= (a[k]/std::pow(z,k)).real();
+    return result;
+}
--- main.cpp
+++ main.cpp
@@ -12,13 +12,22 @@ using std::cout; using std::endl;
 using std::vector;
 
 
-int main() {
+int main(int argc, char** argv) {
     Particles p(N);
     std::string filename="sources.bin";
     read_from_file(p,filename);
-    const int k=5;
-    vector<complex> c=P2E(p,k);
-
+    int k=5;
+    if(argc>1) k=std::atoi(argv[1]);
+    vector<Complex> c(k);
+    {
+        Profiler prof("P2E");
+        c = P2E(p, k);
+    }
+    cout<<"\nExpansion order: "<<k<<endl;
+    Complex z(10,10);
+    double s=E2P(z,c);
+    cout<<"\nStream: "<<s<<endl;
+    cout<<"coeff:"<<endl;
     for(int i=0;i<c.size();i++) cout<<c[i]<<endl;
 }
 
