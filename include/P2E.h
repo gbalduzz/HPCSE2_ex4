1a1f9a6af6b86575554978cda886feefe4e359e9
Giovanni <gbalduzz@itp.phys.ethz.ch>
2016-04-26 18:09:40 +0200

    separatly compile P2E

--- include/P2E.h
+++ include/P2E.h
@@ -3,18 +3,6 @@
 //
 #pragma once
 #include "particles.h"
-#include <vector>
 #include <complex>
-#include <cmath>
-using std::vector;
-using complex = std::complex<double>;
-
-vector<complex> P2E(const Particles& p,const int k){
-    vector<complex> coeff(k+1,0);
-for(int i=0;i<N;i++){
-    coeff[0]+=p.w[i];
-    for(int j=1;j<=k;j++) coeff[j]-=p.w[i]*std::pow(Complex(p.x[i],p.y[i]),j)/complex(j,0);
-}
-    return coeff;
-}
-
+#include <vector>
+std::vector<Complex> P2E(const Particles& p,const int k);
--- include/particles.h
+++ include/particles.h
@@ -6,8 +6,9 @@
 using Complex = std::complex<double>;
 
 struct Particles{
+    const int N; //passed from global scope
     std::vector<double> w,x,y;
-    Particles(int N):x(N),y(N),w(N){}
+    Particles(int N):x(N),y(N),w(N),N(N){}
     };
 
 #endif
