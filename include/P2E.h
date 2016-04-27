99b3be0136479582ed70951abd84679d3996c6f4
Giovanni <gbalduzz@itp.phys.ethz.ch>
2016-04-27 14:15:54 +0200

    optimized diviasion by k

--- include/P2E.h
+++ include/P2E.h
@@ -17,14 +17,14 @@ void P2E(const Particles& p,double* c_re,double* c_im){
 
     for(int i=0;i<p.N;i++){
         c_re[0]+=p.w[i];
-        z_re=1; z_im=0
-                ;
+        z_re=1; z_im=0;
         for(int j=1;j<k;j++){
             z_temp=z_re*p.x[i]-z_im*p.y[i];
             z_im=z_re*p.y[i]+z_im*p.x[i];
             z_re=z_temp;
-            c_re[j]-=p.w[i]*z_re/j;
-            c_im[j]-=p.w[i]*z_im/j;
+            c_re[j]-=p.w[i]*z_re;
+            c_im[j]-=p.w[i]*z_im;
         }
     }
+    for(int i=2;i<k+1;i++) {c_re[i]/=i;c_im[i]/=i;}
 }
