bea24f3cf9e0c626c6bfee8f46e818b24255c4f4
Giovanni <gbalduzz@itp.phys.ethz.ch>
2016-04-27 13:44:24 +0200

    removed complex from E2P

--- include/particles.h
+++ include/particles.h
@@ -1,12 +1,11 @@
 #ifndef PARTICLES_H
 #define PARTICLES_H
 #include <vector>
-#include <complex>
 
 using Complex = std::complex<double>;
 
 struct Particles{
-    const int N; //passed from global scope
+    const int N;
     std::vector<double> w,x,y;
     Particles(int N):x(N),y(N),w(N),N(N){}
     };
--- main.cpp
+++ main.cpp
@@ -1,16 +1,12 @@
 const static int N=1e6;
 const static int k=20;
 #include <iostream>
-#include <vector>
-#include <complex>
-#include <string>
 #include "include/file_IO.h"
 #include "include/particles.h"
 #include "include/profiler.h"
 #include "include/P2E.h"
 #include "include/E2P.h"
 using std::cout; using std::endl;
-using std::vector;
 
 
 int main(int argc, char** argv) {
@@ -23,11 +19,12 @@ int main(int argc, char** argv) {
         Profiler prof("P2E");
         P2E<k>(p,c_re,c_im);
     }
-
-    Complex z(1,1);
-    vector<Complex> c(k+1);
-    for(int i=0;i<k+1;i++) c[i]=Complex(c_re[i],c_im[i]);
-    double s=E2P(z,c);
+    const double z_re=1,z_im=1;
+    double s;
+    {
+        Profiler pr("E2P");
+        s=E2P<k>(z_re,z_im,c_re,c_im);
+    }
     bool correct = check_from_file(s,k,"output.dat");
     if(correct) cout<<"Correct result!"<<endl;
 }
