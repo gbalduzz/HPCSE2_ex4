418c0bc58dd7ee37abb8d688dd13ad42f1cb4209
Giovanni <gbalduzz@itp.phys.ethz.ch>
2016-04-28 14:27:39 +0200

    using #pragma unroll

--- include/P2E.h
+++ include/P2E.h
@@ -7,7 +7,7 @@
 #include <vector>
 #include <cmath>
 
-
+template <int k>
 void P2E( Particles& p,double* c_re,double* c_im){
     double z_re,z_im,z_temp; //will store (x+ i y) ^k
    for(int i=0;i<k+1;i++) c_re[i]=c_im[i]=0;
@@ -15,6 +15,7 @@ void P2E( Particles& p,double* c_re,double* c_im){
     for(int i=0;i<p.N;i++){
         c_re[0]+=p.w[i];
         z_re=1; z_im=0;
+#pragma unroll(k)
         for(int j=1;j<k;j++){
             z_temp=z_re*p.x[i]-z_im*p.y[i];
             z_im=z_re*p.y[i]+z_im*p.x[i];
