80a37a581c10372eff65eded2e20e3e913e69312
Giovanni <gbalduzz@itp.phys.ethz.ch>
2016-05-02 10:34:55 +0200

    temp variable declared as const

--- include/P2E.h
+++ include/P2E.h
@@ -9,15 +9,14 @@
 
 template <int k>
 void P2E( Particles& p,double* c_re,double* c_im){
-    double z_re,z_im,z_temp; //will store (x+ i y) ^k
+    double z_re,z_im; //will store (x+ i y) ^k
    for(int i=0;i<k+1;i++) c_re[i]=c_im[i]=0;
 
     for(int i=0;i<p.N;i++){
         c_re[0]+=p.w[i];
         z_re=1; z_im=0;
-#pragma unroll(k)
         for(int j=1;j<k;j++){
-            z_temp=z_re*p.x[i]-z_im*p.y[i];
+            const double z_temp=z_re*p.x[i]-z_im*p.y[i];
             z_im=z_re*p.y[i]+z_im*p.x[i];
             z_re=z_temp;
             c_re[j]-=p.w[i]*z_re;
